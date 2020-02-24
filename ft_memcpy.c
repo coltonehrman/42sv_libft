cd43a7780443d85763d91f2472c47c892d0a7a2f
Colton Ehrman <cehrman@e1z3r4p1.42.us.org>
2020-02-24 09:15:30 -0800

    better solution for memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: cehrman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 12:42:43 by cehrman           #+#    #+#             */
-/*   Updated: 2020/02/20 17:33:24 by cehrman          ###   ########.fr       */
+/*   Updated: 2020/02/24 09:12:05 by cehrman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char	*ptr;
+	unsigned char	*ptr;
 
 	ptr = dst;
 	if (!dst && !src)
 		return (dst);
 	while (n > 0)
 	{
-		*ptr = *((char *)src);
+		*ptr = *((unsigned char *)src);
 		ptr++;
 		src++;
 		n--;

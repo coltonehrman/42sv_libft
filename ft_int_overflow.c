6bb021dc5d02d8dc8dc86f60ec4ab62a8e6bff36
Colton Ehrman <cehrman@e1z3r4p1.42.us.org>
2020-03-01 06:02:54 -0800

    added function comments for ft_int_overflow

--- ft_int_overflow.c
+++ ft_int_overflow.c
@@ -6,11 +6,20 @@
 /*   By: cehrman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 19:41:31 by cehrman           #+#    #+#             */
-/*   Updated: 2020/02/29 17:00:42 by cehrman          ###   ########.fr       */
+/*   Updated: 2020/03/01 06:02:31 by cehrman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+**		Function	:	ft_int_overflow
+**		Parameters	:	(int)n1, (char)op, (int)n2
+**		Description	:	checks for an (int) overflow when doing
+**						a math operation specified by |n1| |op| |n2|
+**						where |op| can be either '+' or '*'
+**		Return		:	(int) -1 if overflow happens else 0
+*/
 
 int		ft_int_overflow(int n1, char op, int n2)
 {

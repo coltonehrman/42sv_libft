de583fa782e863f662bd7a017edecdfff5d2e67d
Colton Ehrman <coltonje95@gmail.com>
2020-03-01 06:06:54 -0800

    added function comments for ft_isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,11 +6,18 @@
 /*   By: cehrman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 21:13:27 by cehrman           #+#    #+#             */
-/*   Updated: 2020/02/29 17:00:58 by cehrman          ###   ########.fr       */
+/*   Updated: 2020/03/01 06:06:38 by cehrman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+**		Function	:	ft_isalpha
+**		Parameters	:	(int)c
+**		Description	:	checks for if |c| is alpha in ASCII table
+**		Return		:	(int) 1 if is alpha else 0
+*/
 
 int		ft_isalpha(int c)
 {
